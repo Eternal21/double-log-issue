063d912a5ab8c04de18d3dfec6af1cc6f0634ca3
Peter Starzyk <pstarzyk@general-devices.com>
2022-05-13 17:59:45 -0400

    Implemet AddJwtBearer function

--- DoubleLogProject/Program.cs
+++ DoubleLogProject/Program.cs
@@ -7,6 +7,33 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddAuthentication(ApiKeyDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters 
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secret")),
+            // ClockSkew = TimeSpan.Zero // default value is 5 minutes
+        };
+        // see: https://docs.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?view=aspnetcore-5.0#built-in-jwt-authentication
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+        
+                // if the request is for our hub:
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/message-hub"))
+                    context.Token = accessToken;
+                return Task.CompletedTask;
+            }
+        };
+    })
     .AddApiKeyInHeaderOrQueryParams<ApiKeyProvider>(options =>
     {
         options.Realm = "Sample Web API";
